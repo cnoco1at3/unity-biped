52ce4b8322408152b0732f9e49df6e3ba3cd5bfc
ralphlizard <ralph.kim93@gmail.com>
2016-10-15 17:19:27 -0400

    Tango received

---
+++ Assets/Scripts/AutoSkin.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoSkin : MonoBehaviour {
+	[SerializeField] Transform root;
+
+	void Start() {
+		BindMesh ();
+	}
+
+	void BindMesh() {
+		SkinnedMeshRenderer rend = GetComponentInChildren<SkinnedMeshRenderer> ();
+
+		Mesh mesh = (Mesh)Instantiate (GetComponentInChildren<SkinnedMeshRenderer> ().sharedMesh);
+		mesh.RecalculateNormals();
+
+		Transform[] bones = root.GetComponentsInChildren<Transform>();
+		Matrix4x4[] bindPoses = new Matrix4x4[bones.Length];
+
+		for (int i = 0; i < bones.Length; i++) {
+			bindPoses [i] = bones [i].worldToLocalMatrix * transform.localToWorldMatrix;
+		}
+
+		mesh.bindposes = bindPoses;
+
+		BoneWeight[] boneWeights = new BoneWeight[mesh.vertexCount];
+		for (int i = 0; i < mesh.vertices.Length; i++) {
+			int closestIndex = 0;
+			for (int j = 0; j < bones.Length; j++) {
+				Vector3 vert = transform.TransformPoint(mesh.vertices [i]);
+				if (Vector3.Distance (vert, bones [j].position) <
+					Vector3.Distance (vert, bones [closestIndex].position))
+					closestIndex = j;
+			}
+			boneWeights [i].boneIndex0 = closestIndex;
+			boneWeights [i].weight0 = 1;
+		}
+		mesh.boneWeights = boneWeights;
+
+		// Assign bones and bind poses
+		rend.bones = bones;
+		rend.sharedMesh = mesh;
+	}
+}
---
+++ Assets/Scripts/AutoSkinDestructive.cs
@@ -0,0 +1,45 @@
+﻿// 4.x example
+using UnityEngine;
+using System.Collections;
+
+public class AutoSkinDestructive : MonoBehaviour {
+	Transform[] bones;
+	Mesh mesh;
+
+	void Start() {
+		SkinnedMeshRenderer rend = GetComponentInChildren<SkinnedMeshRenderer>();
+
+		mesh = GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh;
+		bones = rend.rootBone.GetComponentsInChildren<Transform>();
+		mesh.boneWeights = new BoneWeight[mesh.vertices.Length];
+
+		for (int i = 0; i < mesh.vertices.Length; i++) {
+			AssignBone (i);
+		}
+
+		Matrix4x4[] bindPoses = new Matrix4x4[bones.Length];
+
+		for (int i = 0; i < bindPoses.Length; i++) {
+			bindPoses [i] = bones [i].worldToLocalMatrix * transform.localToWorldMatrix;
+		}
+
+		mesh.bindposes = bindPoses;
+
+		// Assign bones and bind poses
+		rend.bones = bones;
+		rend.sharedMesh = mesh;
+	}
+
+	void AssignBone (int vertIndex)
+	{
+		print (bones[1]);
+		int closestIndex = 0;
+		for (int i = 1; i < bones.Length; i++) {
+			if (Vector3.Distance(mesh.vertices[vertIndex], bones[i].position) < 
+				Vector3.Distance(mesh.vertices[vertIndex], bones[closestIndex].position))
+				closestIndex = i;
+		}
+		mesh.boneWeights[vertIndex].boneIndex0 = 1;
+		mesh.boneWeights[vertIndex].boneIndex0 = 1;
+	}
+}
